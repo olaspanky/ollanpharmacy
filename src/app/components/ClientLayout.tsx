7805f77ee15bb184ee36f7c17b2babe27e096ea0
Olakareem <olakareemomobolarinwa@gmail.com>
2025-07-07 14:45:53 +0100

    fix

--- src/app/components/ClientLayout.tsx
+++ src/app/components/ClientLayout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import Navbar from "../components/Navbar"; // Adjust path to your Navbar component
+import Navbar from "./Navbar"; // Adjust path to your Navbar component
 
 export default function ClientLayout({
   children,
